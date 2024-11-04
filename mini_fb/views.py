97f38ec44897c271a5df2c05422ffc075168e4da
ethansliu <esliu@bu.edu>
2024-11-04 17:56:21 -0500

    Completed task 3

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from typing import Any
 
@@ -8,6 +8,9 @@ from . models import *
 from . forms import *
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 
 # class-based view
@@ -33,23 +36,41 @@ class CreateProfileView(CreateView):
     def get_login_url(self) -> str:
         '''return the URL required for login'''
         return reverse('login') 
+    
+    def get_context_data(self, **kwargs: Any) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['user_creation_form'] = UserCreationForm()
+        return context
 
-    #def get_absolute_url(self) -> str:
-        #'''return the URL to redirect to after sucessful create'''
- 
-        #return reverse("profile_page", kwargs={'pk': self.object.pk})
+    def form_valid(self, form):
+        ''' Reconstruct the UserCreationForm from submitted POST data'''
+        user_creation_form = UserCreationForm(self.request.POST)
+
+        if user_creation_form.is_valid():
+
+            user = user_creation_form.save()
+
+            login(self.request, user)
+
+            form.instance.user = user
+
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
+
     
 class CreateStatusMessageView(LoginRequiredMixin, CreateView):
 
     form_class = CreateStatusMessageForm
     template_name = "mini_fb/create_status_form.html"
 
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user=self.request.user)
+    
     # what to do after form submission?
-    def get_success_url(self) -> str:
-        '''return the URL to redirect to after sucessful create'''
-        #return "/blog/show_all"
-        #return reverse("show_all")
-        return reverse("profile_page", kwargs=self.kwargs)
+    def get_success_url(self):
+        profile = get_object_or_404(Profile, user=self.request.user)
+        return reverse("profile_page", kwargs={"pk": profile.pk})
     
     def form_valid(self, form):
         '''this method executes after form submission'''
@@ -59,7 +80,7 @@ class CreateStatusMessageView(LoginRequiredMixin, CreateView):
 
         # find the article with the PK from the URL
         # self.kwargs['pk'] is finding the article PK from the URL
-        profile_page = Profile.objects.get(pk=self.kwargs['pk'])
+        profile_page = get_object_or_404(Profile, user=self.request.user)
 
         # attach the article to the new Comment 
         # (form.instance is the new Comment object)
@@ -88,7 +109,7 @@ class CreateStatusMessageView(LoginRequiredMixin, CreateView):
 
         # find the article with the PK from the URL
         # self.kwargs['pk'] is finding the article PK from the URL
-        profile_page = Profile.objects.get(pk=self.kwargs['pk'])
+        profile_page = get_object_or_404(Profile, user=self.request.user)
 
         # add the article to the context data
         context['profile_page'] = profile_page
@@ -107,6 +128,9 @@ class UpdateProfileView(LoginRequiredMixin, UpdateView):
         '''
         print(f'UpdateProfileView: form.cleaned_data={form.cleaned_data}')
         return super().form_valid(form)
+    
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user=self.request.user)
     
 
 class DeleteStatusMessageView(LoginRequiredMixin, DeleteView):
@@ -142,7 +166,7 @@ class UpdateStatusMessageView(LoginRequiredMixin, UpdateView):
 class CreateFriendView(LoginRequiredMixin, View):
     # replace default dispatch
     def dispatch(self, request, *args, **kwargs):
-        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        profile = get_object_or_404(Profile, user=self.request.user)
         other_profile = Profile.objects.get(pk=self.kwargs['other_pk'])
         profile.add_friend(other_profile)        
         return redirect(reverse('profile_page', kwargs={'pk': profile.pk}))
@@ -158,6 +182,9 @@ class ShowFriendSuggestionsView(DetailView):
         context['friend_suggestions'] = self.object.get_friend_suggestions()
         return context
     
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user=self.request.user)
+    
 class ShowNewsFeedView(DetailView):
     model = Profile
     template_name = "mini_fb/news_feed.html"
@@ -168,3 +195,5 @@ class ShowNewsFeedView(DetailView):
         context['news_feed'] = self.object.get_news_feed()
         return context
     
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user=self.request.user)
